4a395075589a76a236ab13cdbb9814f78e4815a0
LeahBasson <leahbasson54@gmail.com>
2024-08-29 13:08:54 +0200

    :building_construction:

--- controller/HotelController.js
+++ controller/HotelController.js
@@ -7,7 +7,7 @@ const hotelRouter = express.Router()
 
 hotelRouter.use(bodyParser.json()) //to not call bodyParser on each and every endpoint, register once using 'router.use' // the body parser is used to pass the body as json. when you sending data from the database you need to have a pipeline body parser is that pipeline // when the user sends a request you need to have a body parser.
 
-hotelRouter.get('/', verifyAToken, (req, res) => {   //a middleware which is verifyAToken is placed between an endpoint and a callback function
+hotelRouter.get('/', (req, res) => {   //a middleware which is verifyAToken is placed between an endpoint and a callback function
     hotels.fetchHotels(req, res) 
 })
 
