11f707c8327d8a390cd03c6530c54baa95823444
LeahBasson <leahbasson54@gmail.com>
2024-09-06 15:37:05 +0200

    :technologist:

--- model/Bookings.js
+++ model/Bookings.js
@@ -28,7 +28,7 @@ class Bookings {
 
     fetchUserBookings(req, res) {
         const strQry = `
-            SELECT b.bookingID, b.hotelID, b.userID, b.numberOfRooms, b.checkInDate, b.checkOutDate, b.totalPrice
+            SELECT b.bookingID, b.hotelID, b.userID, b.numberOfRooms, b.checkInDate, b.checkOutDate, b.totalPrice, h.hotelName
             FROM Bookings b
             INNER JOIN Users u ON b.userID = ${req.params.uid}
             INNER JOIN Hotels h ON b.hotelID = h.hotelID
